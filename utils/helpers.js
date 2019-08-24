b5f65120d9fc54f5f8a05393c939abdf98b2826b
Christopher Tso <christso@gmail.com>
2019-08-24 17:36:38 +1000

    update AddEntry-render

--- utils/helpers.js
+++ utils/helpers.js
@@ -46,7 +46,7 @@ export function getMetricMetaInfo(metric) {
       getIcon() {
         return (
           <View>
-            <MaterialCommunityIcons name="swim" color={white} size={35} />
+            <MaterialCommunityIcons name="swim" color={"black"} size={35} />
           </View>
         );
       }
@@ -60,7 +60,7 @@ export function getMetricMetaInfo(metric) {
       getIcon() {
         return (
           <View>
-            <FontAwesome name="bed" color={white} size={30} />
+            <FontAwesome name="bed" color={"black"} size={30} />
           </View>
         );
       }
@@ -74,7 +74,7 @@ export function getMetricMetaInfo(metric) {
       getIcon() {
         return (
           <View>
-            <MaterialCommunityIcons name="food" color={white} size={35} />
+            <MaterialCommunityIcons name="food" color={"black"} size={35} />
           </View>
         );
       }
