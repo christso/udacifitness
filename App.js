879437b3a2ea27ee3c3c5e1ba2748a8d10576971
Christopher Tso <christso@gmail.com>
2019-08-25 20:50:15 +1000

    fix app style

--- App.js
+++ App.js
@@ -8,18 +8,9 @@ import reducer from './reducers';
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
-      <View style={styles.container}>
+      <View style={{flex: 1}}>
         <AddEntry />
       </View>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
