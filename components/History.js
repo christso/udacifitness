460365427a6919fcdfbd4fd25e597cc59c849870
Christopher Tso <christso@gmail.com>
2019-08-28 15:44:20 +1000

    changes for History-calendar

--- components/History.js
+++ components/History.js
@@ -1,35 +1,57 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { connect } from 'react-redux'
-import { receiveEntries, addEntry } from '../actions'
-import { timeToString, getDailyReminderValue } from '../utils/helpers'
-import { fetchCalendarResults } from '../utils/api'
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { receiveEntries, addEntry } from '../actions';
+import { timeToString, getDailyReminderValue } from '../utils/helpers';
+import { fetchCalendarResults } from '../utils/api';
+import UdaciFitnessCalendar from 'udacifitness-calendar';
 
 class History extends Component {
-  async componentDidMount () {
-    const { dispatch } = this.props
-
+  
+  async componentDidMount() {
+    const { dispatch } = this.props;
+    
     const entries = await fetchCalendarResults();
     dispatch(receiveEntries(entries));
     if (!entries[timeToString()]) {
       dispatch(addEntry({
         [timeToString()]: getDailyReminderValue()
-      }))
+      }));
     }
 
-    this.setState(() => ({ready: true}));
+    this.setState(() => ({ ready: true }));
+  }
+
+  renderItem = ({ today, ...metrics }, formattedDate, key) => (
+    <View>
+      {today
+        ? <Text>{JSON.stringify(today)}</Text>
+        : <Text>{JSON.stringify(metrics)}</Text>}
+    </View>
+  )
+
+  renderEmptyDate(formattedDate) {
+    return (
+      <View>
+        <Text>No Data for this day</Text>
+      </View>
+    )
   }
 
   render() {
+    const { entries } = this.props
+
     return (
-      <View>
-        <Text>{JSON.stringify(this.props)}</Text>
-      </View>
+      <UdaciFitnessCalendar
+        items={entries}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+      />
     )
   }
 }
 
-function mapStateToProps (entries) {
+function mapStateToProps(entries) {
   return {
     entries
   }
