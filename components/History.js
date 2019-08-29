1824a96edaa97d93af9bc2e3c27c1557f9d16b5e
Christopher Tso <christso@gmail.com>
2019-08-29 11:25:09 +1000

    changes for History-methods

--- components/History.js
+++ components/History.js
@@ -1,43 +1,54 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { connect } from 'react-redux';
-import { receiveEntries, addEntry } from '../actions';
-import { timeToString, getDailyReminderValue } from '../utils/helpers';
-import { fetchCalendarResults } from '../utils/api';
-import UdaciFitnessCalendar from 'udacifitness-calendar';
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
+import { connect } from 'react-redux'
+import { receiveEntries, addEntry } from '../actions'
+import { timeToString, getDailyReminderValue } from '../utils/helpers'
+import { fetchCalendarResults } from '../utils/api'
+import UdaciFitnessCalendar from 'udacifitness-calendar'
+import { white } from '../utils/colors'
+import DateHeader from './DateHeader'
 
 class History extends Component {
-  
-  async componentDidMount() {
-    const { dispatch } = this.props;
-    
-    const entries = await fetchCalendarResults();
-    dispatch(receiveEntries(entries));
-    if (!entries[timeToString()]) {
-      dispatch(addEntry({
-        [timeToString()]: getDailyReminderValue()
-      }));
-    }
+  componentDidMount () {
+    const { dispatch } = this.props
 
-    this.setState(() => ({ ready: true }));
+    fetchCalendarResults()
+      .then((entries) => dispatch(receiveEntries(entries)))
+      .then(({ entries }) => {
+        if (!entries[timeToString()]) {
+          dispatch(addEntry({
+            [timeToString()]: getDailyReminderValue()
+          }))
+        }
+      })
+      .then(() => this.setState(() => ({ready: true})))
   }
-
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
-    <View>
+    <View style={styles.item}>
       {today
-        ? <Text>{JSON.stringify(today)}</Text>
-        : <Text>{JSON.stringify(metrics)}</Text>}
+        ? <View>
+            <DateHeader date={formattedDate}/>
+            <Text style={styles.noDataText}>
+              {today}
+            </Text>
+          </View>
+        : <TouchableOpacity
+            onPress={() => console.log('Pressed!')}
+          >
+              <Text>{JSON.stringify(metrics)}</Text>
+          </TouchableOpacity>}
     </View>
   )
-
   renderEmptyDate(formattedDate) {
     return (
-      <View>
-        <Text>No Data for this day</Text>
+      <View style={styles.item}>
+        <DateHeader date={formattedDate}/>
+        <Text style={styles.noDataText}>
+          You didn't log any data on this day.
+        </Text>
       </View>
     )
   }
-
   render() {
     const { entries } = this.props
 
@@ -51,7 +62,32 @@ class History extends Component {
   }
 }
 
-function mapStateToProps(entries) {
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20
+  }
+})
+
+
+function mapStateToProps (entries) {
   return {
     entries
   }
@@ -59,4 +95,4 @@ function mapStateToProps(entries) {
 
 export default connect(
   mapStateToProps,
-)(History) 
+)(History)
