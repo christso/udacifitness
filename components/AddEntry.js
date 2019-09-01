82658cb8f05b5f94e47f7e54a4b0e8c3eedd42c9
Christopher Tso <christso@gmail.com>
2019-09-01 10:59:24 +1000

    Changes for finish-navigation

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -10,6 +10,7 @@ import { submitEntry, removeEntry } from '../utils/api';
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { purple, white } from '../utils/colors';
+import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn({ onPress }) {
   return (
@@ -68,8 +69,9 @@ class AddEntry extends Component {
     }))
 
     this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
-
     // Navigate to home
+    this.toHome()
+    
     submitEntry({ key, entry });
   }
 
@@ -82,13 +84,17 @@ class AddEntry extends Component {
     }))
 
     // Route to Home
-
+    this.toHome();
     // Update "DB"
     
     // Route to Home
     removeEntry(key);
   }
 
+  toHome = () => {
+    this.props.navigation.dispatch(NavigationActions.back({key: 'AddEntry'}))
+  }
+  
   render() {
     const metaInfo = getMetricMetaInfo();
 
